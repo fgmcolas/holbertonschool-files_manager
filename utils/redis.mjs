769e82d2eec3043d05a4fc2c6a2357e5755dc8e7
fgmcolas <https://github.com/fgmcolas>
2024-11-26 19:33:31 +0100

    Task 0

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,11 +1,9 @@
-import { promisify } from 'util';
-
-const redis = require('redis');
+import redis from 'redis';
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.client.on('error', (error) => console.log(error.message));
+    this.client.on('error', (err) => console.log(`Redis error : ${err}`));
   }
 
   isAlive() {
@@ -13,20 +11,39 @@ class RedisClient {
   }
 
   async get(key) {
-    const getval = await promisify(this.client.get).bind(this.client);
-    const val = await getval(key);
-    return val;
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(value);
+      });
+    });
   }
 
-  async set(key, val, duration) {
-    await this.client.set(key, val);
-    await this.client.expire(key, duration);
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 
   async del(key) {
-    await this.client.del(key);
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve();
+      });
+    });
   }
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export { redisClient as default };
