488079d21aa87c5b9ecacc28e5c9f07b70b7bfcd
fgmcolas <https://github.com/fgmcolas>
2024-11-27 15:34:39 +0100

    Task 1

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,5 +1,5 @@
-import mongodb from 'mongodb';
-const MongoClient = require('mongodb').MongoClient;
+import { MongoClient } from 'mongodb';
+const client = new MongoClient(uri);
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
