9045f6bf42f9de0e029521040c9b6333fbf83b3c
fgmcolas <https://github.com/fgmcolas>
2024-11-29 16:05:58 +0100

    Task 2

--- utils/routes/index.js
+++ utils/routes/index.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-const AppController = require('../controllers/AppController');
-
-app.get('/status', (req, res) => {
-  res.send(AppController.getStatus);
-});
-
-app.get('/stats', (req, res) => {
-  res.send(AppController.getStats);
-});
-
+const AppController = require('../controllers/AppController.js');
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats)
 module.exports = router;
