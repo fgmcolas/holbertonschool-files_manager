65b0701ca4c352e247b55fb0e67db0de022c4a11
fgmcolas <https://github.com/fgmcolas>
2024-11-30 11:14:58 +0100

    Task 3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,29 +1,41 @@
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
-class UsersController {
-  static async getMe(req, res) {
-    const token = req.headers['x-token'];
+const postNew = async (req, res) => {
+  const { email, password } = req.body;
 
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const redisKey = `auth_${token}`;
-    const userId = await redisClient.get(redisKey);
-
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const user = await dbClient.usersCollection.findOne({ _id: new dbClient.ObjectId(userId) });
-
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    return res.status(200).json({ id: user._id.toString(), email: user.email });
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' });
   }
-}
+  if (!password) {
+    return res.status(400).json({ error: 'Missing password' });
+  }
 
-export default UsersController;
+  const userEmailExist = await dbClient.userEmailExist(email);
+  if (userEmailExist) {
+    return res.status(400).json({ error: 'Already exist' });
+  }
+  const lastInsert = await dbClient.insertNewUser(email, password);
+  return res.status(201).json(lastInsert);
+};
+
+const getMe = async (req, res) => {
+  const authorizationHeader = req.get('X-Token');
+  if (!authorizationHeader) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  const token = authorizationHeader.trim();
+  const key = `auth_${token}`;
+  const userCacheId = await redisClient.get(key);
+  if (!userCacheId) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  const user = await dbClient.getUserById(userCacheId);
+  if (!user) {
+    return res.status(401);
+  }
+  console.log(user);
+  return res.json(user);
+};
+
+module.exports = { postNew, getMe };
