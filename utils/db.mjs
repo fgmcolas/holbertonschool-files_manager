5e07b7ae3feb80654242b115e5329fc5dbdc0565
fgmcolas <https://github.com/fgmcolas>
2024-11-30 10:50:02 +0100

    Task 3

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,4 +1,5 @@
 import pkg from 'mongodb';
+import crypto from 'crypto';
 
 const { MongoClient } = pkg;
 
@@ -26,13 +27,34 @@ class DBClient {
   }
 
   async nbUsers() {
-    const numberOfUsers = this.usersCollection.countDocuments();
+    const numberOfUsers = await this.usersCollection.countDocuments();
     return numberOfUsers;
   }
 
   async nbFiles() {
-    const numberOfFiles = this.filesCollection.countDocuments();
+    const numberOfFiles = await this.filesCollection.countDocuments();
     return numberOfFiles;
+  }
+
+  async userEmailExist(email) {
+    try {
+      const user = await this.usersCollection.findOne({ email });
+      return !!user;
+    } catch (err) {
+      console.error('Error checking user email existence:', err);
+      return false;
+    }
+  }
+
+  async insertNewUser(email, password) {
+    try {
+      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+      const result = await this.usersCollection.insertOne({ email, password: hashedPassword });
+      return { id: result.insertedId, email };
+    } catch (err) {
+      console.error('Error inserting new user:', err);
+      throw new Error('Failed to insert new user');
+    }
   }
 }
 
