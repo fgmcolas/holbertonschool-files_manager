07daf7da7d09a5955e7a6d4421b7b87e1bdf6e44
fgmcolas <https://github.com/fgmcolas>
2024-12-04 19:52:35 +0100

    Task 1

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,34 +1,103 @@
-import { MongoClient } from 'mongodb';
+import pkg from 'mongodb';
+import crypto from 'crypto';
+
+const { MongoClient, ObjectID } = pkg;
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.client = new MongoClient(`mongodb://${this.host}:${this.port}`, { useUnifiedTopology: true });
-    this.client.connect();
-    this.db = this.client.db(this.database);
+    MongoClient.connect(`mongodb://${host}:${port}`, { useUnifiedTopology: true }, (err, client) => {
+      if (err) {
+        console.log(err.message);
+        this.db = false;
+      }
+      this.db = client.db(database);
+    });
   }
 
   isAlive() {
-    if (this.client.isConnected()) {
-      return true;
-    }
-    return false;
+    return !!this.db;
   }
 
   async nbUsers() {
-    this.db = this.client.db(this.database);
-    const collection = await this.db.collection('users');
-    return collection.countDocuments();
+    const collection = this.db.collection('users');
+    const usersCount = await collection.countDocuments();
+    return usersCount;
   }
 
   async nbFiles() {
-    this.db = this.client.db(this.database);
-    const collection = await this.db.collection('files');
-    return collection.countDocuments();
+    const collection = this.db.collection('files');
+    const filesCount = await collection.countDocuments();
+    return filesCount;
+  }
+
+  async insertNewUser(userEmail, userPass) {
+    const collection = this.db.collection('users');
+    const passHash = crypto.createHash('sha1').update(userPass).digest('hex');
+    const insertResult = await collection.insertOne({ email: userEmail, password: passHash });
+    return { email: userEmail, id: insertResult.insertedId };
+  }
+
+  async userEmailExist(userEmail) {
+    const collection = this.db.collection('users');
+    const email = await collection.findOne({ email: userEmail });
+    return !!(email);
+  }
+
+  async getUser(userEmail, userPass) {
+    const collection = this.db.collection('users');
+    const passHash = crypto.createHash('sha1').update(userPass).digest('hex');
+    const user = await collection.findOne({ email: userEmail, password: passHash });
+    return (user) ? { id: user._id } : false;
+  }
+
+  async getUserById(userId) {
+    const collection = this.db.collection('users');
+    const user = await collection.findOne({ _id: ObjectID(userId) });
+
+    return (user) ? { id: user._id, email: user.email } : false;
+  }
+
+  async getFileById(fileId) {
+    const collection = this.db.collection('files');
+    const file = await collection.findOne({ _id: ObjectID(fileId) });
+    return (file) || false;
+  }
+
+  async insertFiles(file) {
+    const collection = this.db.collection('files');
+    const insert = await collection.insertOne(file);
+    const dbFile = this.getFileById(insert.insertedId);
+    return (dbFile) || false;
+  }
+
+  async getFile(userId, fileId) {
+    const collection = this.db.collection('files');
+    const file = await collection.findOne({ _id: ObjectID(fileId), userId });
+    return (file) || false;
+  }
+
+  async getAllFilesIndex(userId, parentId, page, maxItem) {
+    const collection = this.db.collection('files');
+    const pageInt = parseInt(page, 10);
+    const skip = pageInt * maxItem;
+    const filesIndex = await collection.find({ userId, parentId })
+      .skip(skip).limit(maxItem).toArray();
+    return filesIndex;
+  }
+
+  async updatePublishFileStatus(fileId, fileStatus) {
+    const collection = this.db.collection('files');
+    const update = await collection.updateOne(
+      { _id: ObjectID(fileId) },
+      { $set: { isPublic: fileStatus } },
+    );
+    return (update) || false;
   }
 }
 
 const dbClient = new DBClient();
-module.exports = dbClient;
+export { dbClient as default };
